e64e32587a262671edfd4fc0587a8bb8a6a74ef2
Ho.Tuan.Phat <404Err0rK1@gmail.com>
2024-07-06 17:09:17 +0700

    Draft update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import React from "react";
 import "@/styles/globals.css";
+import "@/styles/globals.css";
 
 import Footer from "@/components/Footer";
 import Navbar from "@/components/navbar/Navbar";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,24 @@ import React from "react";
 import SideBar from "@/components/sidebar/SideBar";
 import DropDownItem from "@/components/elements/DropDownItem";
 import coffeeIcon from "@/public/icons/coffee.svg";
+import HeroSlider from "@/components/heroslider/HeroSlider";
+import StackedList from "@/components/list/StackedList";
+import CartItem from "@/components/elements/CartItem";
 
 export default function Home() {
   const list: React.ReactNode[] = [];
   // create list node -> render list
+
+  const cartItems = Array.from({ length: 4 }, (_, i) => {
+    <CartItem
+      key={i}
+      title="Top Deals"
+      image="/icons/icon-laguage.png"
+      soldNum={55}
+      isDiscount={true}
+      price={189000}
+    />;
+  });
 
   for (let i = 0; i < 15; i++) {
     list.push(
@@ -29,9 +43,42 @@ export default function Home() {
       <div className="flex w-[20%]">
         <SideBar>{list.map((e) => e)}</SideBar>
       </div>
-      {/* //main */}
-      <div className="flex w-[77%] h-[calc(100vh-10rem)] bg-green-400">
-        MAIN BODY
+      <div className="flex flex-col w-[77%] h-[calc(100vh-10rem)] bg-green-400">
+        <HeroSlider />
+        <StackedList>
+          <CartItem
+            title="Top Deals"
+            image="/icons/icon-laguage.png"
+            discountPrice={126000}
+            soldNum={55}
+            isDiscount={true}
+            price={189000}
+          />
+          <CartItem
+            title="Top Deals"
+            image="/icons/icon-laguage.png"
+            discountPrice={126000}
+            soldNum={55}
+            isDiscount={true}
+            price={189000}
+          />
+          <CartItem
+            title="Top Deals"
+            image="/icons/icon-laguage.png"
+            discountPrice={126000}
+            soldNum={55}
+            isDiscount={true}
+            price={189000}
+          />
+          <CartItem
+            title="Top Deals"
+            image="/icons/icon-laguage.png"
+            discountPrice={126000}
+            soldNum={55}
+            isDiscount={true}
+            price={189000}
+          />
+        </StackedList>
       </div>
     </main>
   );
