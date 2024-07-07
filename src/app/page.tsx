0fc205403e23641a3b18efb974c7ed5d483be6fa
Nguyễn Quang Huy <117385142+nqh99@users.noreply.github.com>
2024-07-07 14:29:50 +0700

    Apply Redux to create a layer to manage the state of Eco-Hhb web application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-"use client";
-"use client";
-
-import { useState } from "react";
-import { Dialog, DialogPanel } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import React from "react";
-import Link from "next/link";
-import Footer from "@/components/Footer";
 
 export default function Home() {
- 
+
   return (
     <main className="px-8">
-      MAIN BODY
+      
     </main>
   );
 }
