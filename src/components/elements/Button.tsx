94c504029c22c7ed1e8aaefad512c55ba5c88bc4
Nguyễn Quang Huy <117385142+nqh99@users.noreply.github.com>
2024-07-07 19:49:10 +0700

    Create core components for Eco-Hhb web application

---
+++ src/components/elements/Button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { ButtonHTMLAttributes } from "react";
+import { motion as m, MotionProps } from "framer-motion";
+
+type ButtonProps = MotionProps & ButtonHTMLAttributes<HTMLElement>;
+
+/**
+ * Button Component.
+ * This component support all features that a button html supported and can be styled with motion. It should contain a word/string or a icon inside its.
+ * Some attributes that you should pay attention to such as: value, children, type, onClick, ...
+ * Use case: 
+ * - use with form
+ * - in case, it isn't linked to anything url
+ * - .. etc
+ * Example: [Submit], [x icon]
+ *
+ * @component
+ * @param {ButtonProps} props - The button element props.
+ * @returns {ReactNode} The rendered button element.
+ */
+const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
+  return (
+    <m.button
+      whileHover={{ scale: 0.9 }}
+      className={`box-border inline-flex items-center justify-center gap-3 rounded-md bg-white font-medium p-1 text-green-600 ${className}`}
+      {...props}
+    >
+      {children}
+    </m.button>
+  );
+};
+
+export default Button;
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -3,31 +3,34 @@
  * @param string - The input string.
  * @returns The input string with the first letter of each word capitalized.
  */
-export function capitalizeWords(str: string): string {
-    const words = str.replace(/-/g, " ").split(" ");
-    const capitalizedWords = words.map((word) => {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-    });
-    const capitalizedJoined = capitalizedWords.join(" ");
-    return capitalizedJoined;
+const capitalizeWords = (str: string): string => {
+  const words = str.replace(/-/g, " ").split(" ");
+  const capitalizedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  const capitalizedJoined = capitalizedWords.join(" ");
+  return capitalizedJoined;
 }
 
 /**
  * Converts a string to a URL-friendly format.
- * 
+ *
  * @param string - The string to be converted.
  * @returns The converted URL string.
  */
-export function convertToURL(str: string): string {
-    const stringWithHyphens = str.toLocaleLowerCase().replace(/\s+/g, "-");
-    const stringWithoutSpecialCharacters = stringWithHyphens
-        .replace(/(?<![a-zA-Z])[0-9%]+/g, "")
-        .replace(/\-+/g, "-").replace("&", "and");
+const convertToURL = (str: string): string => {
+  const stringWithHyphens = str.toLocaleLowerCase().replace(/\s+/g, "-");
+  const stringWithoutSpecialCharacters = stringWithHyphens
+    .replace(/(?<![a-zA-Z])[0-9%]+/g, "")
+    .replace(/\-+/g, "-")
+    .replace("&", "and");
 
-    const url =
-        stringWithoutSpecialCharacters.charAt(0) === "-"
-            ? stringWithoutSpecialCharacters.substring(1)
-            : stringWithoutSpecialCharacters;
+  const url =
+    stringWithoutSpecialCharacters.charAt(0) === "-"
+      ? stringWithoutSpecialCharacters.substring(1)
+      : stringWithoutSpecialCharacters;
 
-    return url;
+  return url;
 }
+
+export { capitalizeWords, convertToURL };
