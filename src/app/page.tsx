f5f53f0046fd0dd3fd76f0224e8e9e755e7fc9cd
Huy Q. Nguyen <huyq.nguyen@tx-tech.com>
2024-07-09 10:05:33 +0700

    Draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Checkbox from "@/components/elements/Checkbox";
+import { TvIcon } from "@heroicons/react/16/solid";
+import { CheckIcon } from "@heroicons/react/20/solid";
 import React from "react";
 
 export default function Home() {
   return (
     <main className="px-8">
-      <Checkbox />
+      <Checkbox>
+        {/* <CheckIcon className="hidden size-4 fill-green-950 group-data-[checked]:block" /> */}
+      </Checkbox>
     </main>
   );
 }
--- src/components/elements/Checkbox.tsx
+++ src/components/elements/Checkbox.tsx
@@ -4,16 +4,37 @@ import { Checkbox as ICheckBox } from "@headlessui/react";
 import { CheckIcon } from "@heroicons/react/16/solid";
 import { useState } from "react";
 
-export default function Checkbox() {
+/**
+ * Checkbox Component.
+ * Likely checkbox in html but we already styled it. If you want to custom this component, please change its attributes correctly.
+ *
+ * @param icon - The icon to display inside the checkbox.
+ * @param style - The style of the checkbox.
+ */
+export default function Checkbox({
+  children,
+  style,
+}: {
+  children?: React.ReactNode;
+  style?: string;
+}) {
   const [enabled, setEnabled] = useState(false);
 
   return (
     <ICheckBox
       checked={enabled}
       onChange={setEnabled}
-      className="group size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset data-[checked]:bg-white"
+      className={
+        typeof style === "string"
+          ? style
+          : "inline-block border group data-[checked]:bg-green-100 data-[checked]:border-0 bg-white/100 ring-1 ring-white/15 ring-inset size-4 rounded-md"
+      }
     >
-      <CheckIcon className="hidden size-4 fill-black group-data-[checked]:block" />
+      {children ? (
+        children
+      ) : (
+        <CheckIcon className="hidden size-4 fill-green-950 group-data-[checked]:block" />
+      )}
     </ICheckBox>
   );
 }
