e65f288dc3b3af2484f8c3720db25bc3fb17c946
Huy Q. Nguyen <huyq.nguyen1999@gmail.com>
2024-07-13 14:47:41 +0700

    Create footer section for home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import React from "react";
+import "@/styles/globals.css";
 
 import Footer from "@/components/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
