84224182d85c9af8723148b63fa21634c39a99c7
Huy Q. Nguyen <huyq.nguyen1999@gmail.com>
2024-08-15 11:11:23 +0700

    Draft

---
+++ src/app/checkout/payment/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PaymentPage = () => {
+  return (
+    <div className="p-default">
+      <div>
+        <h2>Thanh Toán</h2>
+      </div>
+      <div className="flex flex-row gap-3">
+        <div>
+          {/* Payment methods */}
+          <div></div>
+          {/* Cart Products */}
+          <div></div>
+        </div>
+        {/* Summary Payment Information */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentPage;
